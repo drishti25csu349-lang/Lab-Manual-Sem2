6e3b6cc3f3770cb68851ef545d8073b92a9a4551
Drishti Saini <drishti25csu349@ncuindia.edu>
2026-05-04 08:56:36 +0530

    Create ques33.cpp

---
+++ ques33.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int marks[3][5];
+
+    for (int i = 0; i < 3; i++) {
+        cout << "Enter marks in 5 subjects for student " << i + 1 << ":" << endl;
+        for (int j = 0; j < 5; j++) {
+            cin >> marks[i][j];
+        }
+    }
+
+  
+    cout << "Marks in 2nd subject of 1st student: " << marks[0][1] << endl;
+    cout << "Marks in 5th subject of 3rd student: " << marks[2][4] << endl;
+
+
+    return 0;
+}
