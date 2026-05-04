f9303110b86416ad32f8b43ec85b5a66c66d439c
Drishti Saini <drishti25csu349@ncuindia.edu>
2026-05-04 08:55:11 +0530

    Create ques36.cpp

---
+++ ques36.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int matrix[3][3];
+    int transpose[3][3];
+
+    cout << "Enter the elements of the 3x3 matrix:" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    cout << "\nOriginal Matrix:" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "\nTranspose Matrix:" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << transpose[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
