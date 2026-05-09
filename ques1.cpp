7e570b2b37f9926f4d059bf790da8d968c7d543f
Drishti Saini <drishti25csu349@ncuindia.edu>
2026-05-09 12:08:33 +0530

    Create ques1.cpp

---
+++ ques1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double m1, m2, m3;
+    cin >> m1 >> m2 >> m3;
+    double avg = (m1 + m2 + m3) / 3.0;
+    cout << avg << endl;
+    return 0;
+}
