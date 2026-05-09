4607dddcdc890ed845d6072407a7d7f9d24436cf
Drishti Saini <drishti25csu349@ncuindia.edu>
2026-05-09 12:08:50 +0530

    Create ques2.cpp

---
+++ ques2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double radius;
+    cin >> radius;
+    double area = 3.14159265359 * radius * radius;
+    cout << area << endl;
+    return 0;
+}
