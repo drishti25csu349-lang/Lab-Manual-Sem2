156816e47b199a6fd57b8c14fe1793c78b582b5f
Drishti Saini <drishti25csu349@ncuindia.edu>
2026-05-09 12:11:06 +0530

    Create ques7.cpp

---
+++ ques7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+    if (s1 >= s2 && s1 >= s3) cout << s1 << endl;
+    else if (s2 >= s1 && s2 >= s3) cout << s2 << endl;
+    else cout << s3 << endl;
+    return 0;
+}
+
